52724271582578ac30d936dc89b76ffe26b7b1b4
nayudurakeshkumar <nayudurakesh.k@gmail.com>
2024-10-29 18:44:45 +0000

    Init commit

---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,34 @@
+export function TodoCard(props) {
+  const { todo, handleDeleteTodo, todoIndex, handleCompleteTodo, handleEditTodo } = props;
+
+  return (
+    <div className="card todo-item">
+      <p>{todo.input}</p>
+      <div className="todo-buttons">
+        <button
+          onClick={() => {
+            handleCompleteTodo(todoIndex);
+          }}
+          disabled={todo.complete}
+        >
+          <h6>Done</h6>
+        </button>
+        <button
+          onClick={() => {
+            handleDeleteTodo(todoIndex);
+          }}
+        >
+          <h6>Delete</h6>
+        </button>
+        <button
+          onClick={() => {
+            handleEditTodo(todoIndex);
+          }}
+          disabled={todo.complete}
+        >
+          <h6>Edit</h6>
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,27 @@
+
+export function TodoInput(props) {
+  const { handleAddTodo, inputValue, setInputValue } = props;
+
+  return (
+    <div className="input-container">
+      <input
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+        placeholder="Add task"
+      />
+      <button
+        onClick={() => {
+          if (!inputValue) {
+            return;
+          }
+          handleAddTodo(inputValue);
+          setInputValue("");
+        }}
+      >
+        <i className="fa-solid fa-plus"></i>
+      </button>
+    </div>
+  );
+}
